5692a5ae63e64aa5041ece78a88a8cc1fe337599
Santosh Maurya <SantoshMaurya@Ashutoshs-MacBook-Air.local>
2017-07-26 00:01:03 +0530

    Like, dislike

--- iOS/RankAppSwift/MytripContants.swift
+++ iOS/RankAppSwift/MytripContants.swift
@@ -14,12 +14,15 @@ import Foundation
 
 //Live servicer URL
 //let kSERVER_URL:String = "http://35.154.170.159"
-let kSERVER_URL:String = "http://13.126.211.132" //"http://132.148.82.33:3004"
+//let kSERVER_URL:String = "http://13.126.211.132" //"http://132.148.82.33:3004"
+let kSERVER_URL:String = "http://api.rankkk.com/" //"http://132.148.82.33:3004"
 let kChAT_SERVER_URL:String = "http://52.76.23.63:3000" //"http://54.255.176.61:3004"
 
 let kDEVICE_TYPE_VALUE:String = "I"
 let kTRIP_REGISTRATION:String = "/trip_register"
 let kTRIP_LOGIN:String = "/trip_login"
 let kTRIP_TRIP_POST:String = "/trip_post"
-
+let kTRIP_COMMENT_POST:String = "/trip_post/comments/store"
+let kTRIP_LIKE_POST:String = "/trip_like/1?id=30"
+let kTRIP_DISLIKE_POST:String = "/trip_dislike/1?id=30"
 //http://13.126.211.132/trip_login?email=c.a.r@projectsadda.in&amp;password=prakash
